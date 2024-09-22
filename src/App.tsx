75a95772a20c3e6d23e9ae7d880225ff022af94a
Ravindranath K <ravindranath.k@gmail.com>
2024-09-22 20:34:31 +0530

    update select

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const noop = () => {};
+
+  const [area, setArea] = useState("marvel");
+  const [role, setRole] = useState("user");
+
+  const handleAreaChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setArea(event.target.value);
+  };
+
+  const handleRoleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setRole(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <nav className="navbar">
+          <div className="logo">Co-Pilot</div>
+        </nav>
+
+        <div className="login-box">
+          <h2>Welcome user!</h2>
+          <form action="">
+            <div className="input-wrapper">
+              <label htmlFor="area-select">Area: </label>
+              <select id="area-select" value={area} onChange={handleAreaChange}>
+                <option value="marvel">Marvel</option>
+                <option value="dc">DC Comics</option>
+              </select>
+            </div>
+            <div className="input-wrapper">
+              <label htmlFor="role-select">Role: </label>
+              <select id="role-select" value={role} onChange={handleRoleChange}>
+                <option value="admin">Role Admin</option>
+                <option value="superadmin">Super Admin</option>
+                <option value="user">Role user</option>
+              </select>
+            </div>
+            <div className="input-wrapper">
+              <input type="submit" value="Login" />
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
